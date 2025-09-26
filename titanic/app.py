6e093f079a0f0c0df286995370a31ec879c488da
Bijay Syangtan <justbijay48@gmail.com>
2025-09-26 10:40:52 +0545

    Added Custom LR + titanic UI

--- titanic/app.py
+++ titanic/app.py
@@ -5,28 +5,30 @@ import numpy as np
 app = Flask(__name__)
 
 rf_model = pickle.load(open('artifacts/rf_model.pkl', 'rb'))
-rf_model = pickle.load(open('artifacts/rf_model.pkl', 'rb'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction = None
+    inp_classes  = {}
+
     if request.method == 'POST':
-        # Get form data
-        Pclass = int(request.form['Pclass'])
-        Sex = int(request.form['Sex'])
-        Age = float(request.form['Age'])
-        Fare = float(request.form['Fare'])
-        Embarked = int(request.form['Embarked'])
-        Family = int(request.form['Family'])
+        try:
+            inp_classes = {
+                'Pclass': int(request.form.get('Pclass', 3)),
+                'Sex': int(request.form.get('Sex', 0)),
+                'Age': float(request.form.get('Age', 30)),
+                'Embarked': int(request.form.get('Embarked', 0)),
+                'FamilyGroup': int(request.form.get('Family', 0))
+            }
 
-        # Prepare features (no scaling needed)
-        features = np.array([[Pclass, Sex, Age, Fare, Embarked, Family]])
+            features = np.array([[v for v in inp_classes.values()]])
 
-        # Make prediction
-        pred = rf_model.predict(features)
-        prediction = "Survived" if pred[0] == 1 else "Did not survive"
+            pred = rf_model.predict(features)
+            prediction = "✅ Survived" if pred == 1 else "❌ Did not survive"
 
-    return render_template('index.html', prediction=prediction)
+        except Exception as e:
+            prediction = f"Error : {str(e)}"
+    return render_template('index.html', prediction=prediction, input_data = inp_classes)
 
 if __name__ == '__main__':
     app.run(debug=True)
